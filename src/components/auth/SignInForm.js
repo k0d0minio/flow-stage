a75f2173a85549da41de720c80a85c344cd7631a
Jamie Nisbet <jamie.nisbet@outlook.be>
2025-09-24 22:04:50 +0100

    supabase auth

--- src/components/auth/SignInForm.js
+++ src/components/auth/SignInForm.js
@@ -116,7 +116,7 @@ export default function SignInForm() {
 
       <div className="mt-6 text-center">
         <p className="text-sm text-gray-600">
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <a href="/auth/signup" className="font-medium text-blue-600 hover:text-blue-500">
             Sign up
           </a>
